3f8306be64b766463b44a3aa9b3cdf3a48bcc01d
JBTC10106 <JBTC10106@JBTC10106-PC>
2018-08-30 15:34:32 +0900

    02

---
+++ 자바기초실습/src/제02차시/Class01.java
@@ -0,0 +1,22 @@
+package 제02차시;
+
+class Class01{
+	int i=0;
+	
+	
+	Class01()
+	{
+		i+=3;
+		System.out.println("난 Class01 클래스의 생성자란다.");
+		System.out.println("Class 01 클래스의 모양으로 객체를 만든 즉시 호출됐지.");
+		System.out.println("난 i, 필드 또는 맴버 변수라고도 하지. c언어의 전역 변수 정도로 이해하자.");
+
+		
+		
+		
+	}
+	
+	
+	
+	
+}
